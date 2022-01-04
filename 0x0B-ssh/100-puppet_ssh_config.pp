21a41fd276d63d82dba790eac86eb8e8befa5b10
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-04 10:25:44 -0500

    ADD: puppet create file ssh_config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,10 @@
+# configuration ssh config file
+$str = "Host *
+        ForwardAgent yes
+        ForwardX11 yes
+        IdentityFile ~/.ssh/holberton
+"
+file {'ssh config': 
+  path    => '/etc/ssh/ssh_config',
+  content => $str,
+}
