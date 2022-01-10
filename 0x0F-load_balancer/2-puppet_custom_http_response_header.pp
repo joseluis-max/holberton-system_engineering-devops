ef1b0e7300799e3209a81b428229be83f1f156ee
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-10 18:31:31 -0500

    ADD: puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -3,17 +3,17 @@ exec {'update':
     command  => 'sudo apt-get update',
     provider => shell,
 }
-package {'nginx':
-    ensure => 'present',
+-> package {'nginx':
+    ensure => present,
 }
-file_line { 'header line':
+-> file_line { 'header line':
   ensure => present,
   path   => '/etc/nginx/sites-available/default',
   line   => "	location / {
   add_header X-Served-By ${hostname};",
   match  => '^\tlocation / {',
 }
-exec { 'restart service':
+-> exec { 'restart service':
   command  => 'sudo service nginx restart',
   provider => shell,
 }
