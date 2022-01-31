299d6695dc458e81a354fbf3c9f07d5d2590ecf6
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-31 14:18:17 -0500

    UPDATE: pycodestyle styles

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,29 +1,32 @@
 #!/usr/bin/python3
-""" script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress. """
+""" script that, using this REST API, for a given employee ID,
+    returns information about his/her TODO list progress.
+"""
 import json
 from sys import argv
 import urllib.request
 
 from numpy import take
 
-with urllib.request.urlopen(f"https://jsonplaceholder.typicode.com/users/{argv[1]}") as response:
+with urllib.request.urlopen(f"https://jsonplaceholde\
+                             typicode.com/users/{argv[1]}") as response:
     html = response.read()
     html = json.loads(html.decode())
     name = html.get("name")
     print(f"Employee {name} is done with ", end="")
 
-with urllib.request.urlopen(f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos") as response:
+with urllib.request.urlopen(f"https://jsonplaceholder.\
+                             typicode.com/users/{argv[1]}/todos") as response:
     html = response.read()
     html = json.loads(html.decode())
     done = 0
     task = 0
     for t in html:
-        if t.get("completed") == True:
+        if t.get("completed") is True:
             done += 1
         task += 1
     print(f"tasks({done}/{task}):")
 
     for t in html:
-        if t.get("completed") == True:
+        if t.get("completed") is True:
             print("\t {}".format(t.get("title")))
-
