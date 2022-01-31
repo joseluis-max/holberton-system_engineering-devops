ec63ca65c9a66b84a8060e3b75b45f64f345e1ed
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-31 16:05:12 -0500

    UPDATE: Docstring

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+# API
 """ Script that, using this REST API, for a given employee ID,
     returns information about his/her TO DO list progress.
 """
@@ -7,25 +8,26 @@ from sys import argv
 from urllib import request
 
 
-url_user = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
-with request.urlopen(url_user) as response:
-    html = response.read()
-    html = json.loads(html.decode())
-    name = html.get("name")
-    print(f"Employee {name} is done with ", end="")
+if __name__ == "__main__":
+    url_user = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    with request.urlopen(url_user) as response:
+        html = response.read()
+        html = json.loads(html.decode())
+        name = html.get("name")
+        print(f"Employee {name} is done with ", end="")
 
-url_task = f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
-with request.urlopen(url_task) as response:
-    html = response.read()
-    html = json.loads(html.decode())
-    done = 0
-    task = 0
-    for t in html:
-        if t.get("completed") is True:
-            done += 1
-        task += 1
-    print(f"tasks({done}/{task}):")
+    url_task = f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
+    with request.urlopen(url_task) as response:
+        html = response.read()
+        html = json.loads(html.decode())
+        done = 0
+        task = 0
+        for t in html:
+            if t.get("completed") is True:
+                done += 1
+            task += 1
+        print(f"tasks({done}/{task}):")
 
-    for t in html:
-        if t.get("completed") is True:
-            print("\t {}".format(t.get("title")))
+        for t in html:
+            if t.get("completed") is True:
+                print("\t {}".format(t.get("title")))
