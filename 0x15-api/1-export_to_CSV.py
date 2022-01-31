6fb1e86020c0a64646e83c4a40c7c9d043d040a8
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-01-31 17:38:34 -0500

    UPDATE: f by format

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,11 +9,13 @@ from urllib import request
 
 
 if __name__ == "__main__":
-    url_user = f"https://jsonplaceholder.typicode.com/users/{argv[1]}"
+    url_user = "https://jsonplaceholder.typicode.com/users/{}"\
+                .format({argv[1]})
     with request.urlopen(url_user) as response:
         user = json.loads(response.read().decode())
 
-        url_task = f"https://jsonplaceholder.typicode.com/users/{argv[1]}/todos"
+        url_task = "https://jsonplaceholder.typicode.com/users/{}/todos"\
+                   .format({argv[1]})
         with request.urlopen(url_task) as response:
             tasks = json.loads(response.read().decode())
             with open("{}.csv".format(user.get("id")), "w") as csvfile:
