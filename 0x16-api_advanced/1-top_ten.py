fe25e73c7876fd2dc1d9a0cb87f7d00a24ce3001
José Valdiviezo <luisj.valdiviezo20@gmail.com>
2022-02-16 10:45:32 -0500

    CREATE: top_ten module

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" displays the 10 hot reddit posts in a specified subreddit """
+import requests
+
+
+def top_ten(subreddit):
+    """ Queries the Reddit API and prints the titles """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    user_agent = {'User-Agent': 'Fredhii'}
+    req = requests.get(url, headers=user_agent, allow_redirects=False,
+                       params={'limit': 10})
+    if req.status_code == 200:
+        req = req.json()
+        data = req.get('data')
+        children = data.get('children')
+        if data is not None and children is not None:
+            for post in children:
+                post_data = post.get('data')
+                title = post_data.get('title')
+                print(title)
+    else:
+        print('None')
